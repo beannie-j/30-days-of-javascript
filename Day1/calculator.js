87eaad01905c9ff3b97f886c78f5f8acb605479a
Jeannie An <jeanniehjan@gmail.com>
2020-12-05 20:07:12 +0000

    first commit

---
+++ Day1/calculator.js
@@ -0,0 +1,5 @@
+const numberButtons = document.querySelectorAll(".number");
+const operationButtons = document.querySelectorAll(".operator");
+
+console.log(numberButtons);
+console.log(operationButtons);
