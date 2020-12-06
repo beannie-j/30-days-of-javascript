99a94a3ba5500e770f784c36d267b00135accced
Jeannie An <jeanniehjan@gmail.com>
2020-12-06 13:11:13 +0000

    calculator working

--- Day1/calculator.js
+++ Day1/calculator.js
@@ -1,5 +1,57 @@
 const numberButtons = document.querySelectorAll(".number");
 const operationButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector(".equal-sign");
+const clearButton = document.querySelector(".all-clear");
+const previousOperandTextElement = document.querySelector(".previous-operand");
+const currentOperandTextElement = document.querySelector(".current-operand");
 
-console.log(numberButtons);
-console.log(operationButtons);
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.currentOperand = "";
+        this.previousOperand = "";
+        this.operation = undefined;
+    }
+
+    clear() {
+        this.currentOperand = "";
+        this.previousOperand = "";
+        this.operation = undefined;
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+
+    appendNumber(number) {
+        if (number === ".") return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+        console.log(this.currentOperand);
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    render() {
+        this.currentOperandTextElement.value = this.currentOperand;
+        console.log("render: " + this.currentOperand);
+        console.log("innerText: " + this.currentOperandTextElement.value);
+    }
+
+    update() {
+        console.log("update: " + this.currentOperand);
+    }
+}
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        let val = button.innerText;
+        calculator.appendNumber(val);
+        calculator.update();
+        calculator.render();
+    })
+});
