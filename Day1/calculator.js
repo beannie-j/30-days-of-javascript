39da17f922d292affe255187ed9062709b285694
Jeannie An <jeanniehjan@gmail.com>
2020-12-06 16:53:00 +0000

    added bracket calculation

--- Day1/calculator.js
+++ Day1/calculator.js
@@ -1,9 +1,16 @@
 const numberButtons = document.querySelectorAll(".number");
 const operationButtons = document.querySelectorAll(".operator");
 const equalsButton = document.querySelector(".equal-sign");
-const clearButton = document.querySelector(".all-clear");
+const clearButton = document.querySelector(".clear");
 const previousOperandTextElement = document.querySelector(".previous-operand");
 const currentOperandTextElement = document.querySelector(".current-operand");
+const openBracketButton = document.querySelector(".open-bracket");
+const closeBracketButton = document.querySelector(".close-bracket");
+
+function isNumeric(str) {
+    if (typeof str != "string") return false;
+    return !isNaN(str) && !isNaN(parseFloat(str));
+}
 
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
@@ -11,13 +18,26 @@ class Calculator {
         this.currentOperandTextElement = currentOperandTextElement;
         this.currentOperand = "";
         this.previousOperand = "";
-        this.operation = undefined;
+        this.operator = undefined;
+        this.isBracketOpen = false;
+        this.isBrachetClose = false;
+        this.isComputeCompleted = false;
     }
 
     clear() {
         this.currentOperand = "";
         this.previousOperand = "";
-        this.operation = undefined;
+        this.operator = undefined;
+        this.isBracketOpen = false;
+        this.isBrachetClose = false;
+    }
+
+    changeACtoCE() {
+        clearButton.innerHTML = "CE";
+    }
+
+    changeCEtoAC() {
+        clearButton.innerHTML = "AC";
     }
 
     delete() {
@@ -26,22 +46,117 @@ class Calculator {
 
     appendNumber(number) {
         if (number === ".") return;
+        this.changeACtoCE();
         this.currentOperand = this.currentOperand.toString() + number.toString();
-        console.log(this.currentOperand);
     }
 
-    chooseOperation(operation) {
-
+    appendOperator(operator) {
+        if (operator === "") return;
+        this.currentOperand = this.currentOperand.toString() + " " + operator.toString() + " ";
     }
 
-    render() {
-        this.currentOperandTextElement.value = this.currentOperand;
-        console.log("render: " + this.currentOperand);
+    chooseOperation(operator) {
+        if (this.currentOperand === "") return;
+        this.operator = operator;
+        this.appendOperator(operator);
+    }
+
+    appendOpenBracket(bracket) {
+        const functionToEvaluate = this.currentOperand;
+        let arr = functionToEvaluate.split(" ");
+        console.log(arr);
+
+        function isOperand(operand) {
+            if (operand == '×') return true;
+            if (operand == '+') return true;
+            if (operand == '-') return true;
+            if (operand == '%') return true;
+            if (operand == '÷') return true;
+        } 
+        let last = arr[arr.length - 2];    
+        if (isOperand(last)) {
+            this.isBracketOpen = true;
+            this.currentOperand = this.currentOperand.toString() + " " + bracket + " ";
+        }   
+    }
+
+    appendCloseBracket(bracket) {
+        const functionToEvaluate = this.currentOperand;
+        let arr = functionToEvaluate.split(" ");
+        console.log(arr);
+        let last = arr[arr.length - 1];
+        console.log("last elem " + last);
+        if (isNumeric(last) && this.isBracketOpen) {
+            this.isBracketClose = true;
+            this.currentOperand = this.currentOperand.toString() + " " + bracket + " ";
+            this.isBracketClose = false;
+            this.isBracketOpen = false;
+        }   
+    }
+
+    render() { 
         console.log("innerText: " + this.currentOperandTextElement.value);
     }
 
     update() {
-        console.log("update: " + this.currentOperand);
+        this.currentOperandTextElement.value = this.currentOperand;
+        this.previousOperandTextElement.value = this.previousOperand;
+    }
+
+    compute() {
+        const functionToEvaluate = this.currentOperand;
+        let arr = functionToEvaluate.split(" ");
+        console.log(arr);
+        let computedValue = 0;
+        let prev = parseFloat(arr[0]);
+        let curr = 0;
+        let prevOperator = "";
+        
+        for (let i = 1; i < arr.length; i++) {
+            console.log(prev);
+            console.log(curr);
+            if (arr[i] === "" || arr[i] === " ") continue;
+            if (isNumeric(arr[i])) {
+                curr = parseFloat(arr[i]);
+                console.log("curr " + curr);
+                switch (prevOperator) {
+                    case "×":
+                        computedValue = prev * curr;
+                        prev = computedValue;
+                        console.log("val " + computedValue);
+                        break;
+                    case "+":
+                        computedValue = prev + curr;
+                        prev = computedValue;
+                        console.log("val " + computedValue);
+                        break;
+                    case "-":
+                        computedValue = prev - curr;
+                        prev = computedValue;
+                        console.log("val " + computedValue);
+                        break;
+                    case "÷":
+                        computedValue = prev / curr;
+                        prev = computedValue;
+                        console.log("val " + computedValue);
+                        break;
+                    case "%":
+                        computedValue = prev % curr;
+                        prev = computedValue;
+                        console.log("val " + computedValue);
+                        break;
+                }
+            }
+            else {
+                prevOperator = arr[i];
+                console.log(prevOperator);
+            }
+        }
+        console.log(functionToEvaluate);
+        this.previousOperand = functionToEvaluate;
+        this.currentOperand = computedValue;
+        this.isComputeCompleted = true;
+        this.changeCEtoAC();
     }
 }
 
@@ -53,5 +168,45 @@ numberButtons.forEach(button => {
         calculator.appendNumber(val);
         calculator.update();
         calculator.render();
-    })
+    });
+});
+
+operationButtons.forEach(button => {
+    if (this.currentOperand == "") return;
+    button.addEventListener('click', () => {
+        let operator = button.innerText;
+        console.log(operator);
+        calculator.chooseOperation(operator);
+        calculator.update();
+        calculator.render();
+    });
+});
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.update();
+    calculator.render();
+});
+
+clearButton.addEventListener('click', button => {
+    let clearAction = clearButton.innerHTML;
+    if (clearAction == "CE") calculator.delete();
+    if (clearAction == "AC") calculator.clear();
+    calculator.update();
+    calculator.render();
+});
+
+openBracketButton.addEventListener('click', button => {
+    console.log("opening bracket");
+    calculator.appendOpenBracket("(");
+    calculator.update();
+    calculator.render();
+});
+
+closeBracketButton.addEventListener('click', button => {
+    if (!calculator.isBracketOpen) return;
+    console.log("closing bracket");
+    calculator.appendCloseBracket(")");
+    calculator.update();
+    calculator.render();
 });
