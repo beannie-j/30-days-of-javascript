bb06acb185089c3de59234156358a11fe5b6b4af
Jeannie An <jeanniehjan@gmail.com>
2020-12-09 18:39:10 +0000

    fixed rendering multiple balls

--- Day4/bouncingBalls.js
+++ Day4/bouncingBalls.js
@@ -1,5 +1,3 @@
-// https://codepen.io/b4rb4tron/pen/wjyXNJ
-console.log("hello bouncing balls");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -11,10 +9,6 @@ const windowWidth = window.innerWidth;
 canvas.width = windowWidth;
 canvas.height = windowHeight;
 
-// function isCanvasSupported() {
-//     console.log(!!ctx);
-//     return !!ctx;
-// }
 function getRandomColor() {
     let colors =["#242582", "#553D67", "#F64C72", "#997382", "#2F2FA2", "#E76F51", "#FADA5E"];
     let num = getRandomInt(0, colors.length);
@@ -25,25 +19,23 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
+}
+
+let balls = [];
 
 class Ball {
-    static gravity = 0.2;   // gravity
-    static factor = 0.9;    // velocity reduction factor per bounce  
     constructor() {
         this.radius = getRandomInt(15, 36);
-        // this.x = getRandomInt(this.radius, windowWidth);
-        this.x = this.radius;
+        this.x = getRandomInt(this.radius, windowWidth);
         this.y = this.radius;
         this.color = getRandomColor();
-        this.vx = 1;    // velocity of ball
-        this.vy = 0;
-        this.isPaused = false;
-        console.log("new ball created");
+        this.dx = 1;
+        this.dy = 1;
+        this.velocity = 0.2;
+        this.gravity = 0.99;
     }
 
     draw() {   
-        ctx.clearRect(0, 0, windowWidth, windowHeight);
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
         ctx.fillStyle = this.color;
@@ -52,34 +44,34 @@ class Ball {
     }
 
     update() {
-        // update velocity
-        this.vy += Ball.gravity;
-        // update position
-        this.x += this.vx;
-        this.y += this.vy;
-        // handle bouncing
-        if (this.y > windowHeight - this.radius) {
-            this.y = windowHeight - this.radius;
-            this.vy *= -Ball.factor;
+        this.y += this.dy;
+        this.x += this.dx;
+        if (this.y + this.radius >= windowHeight) {
+            this.dy = -this.dy * this.gravity;
+        } else {
+            this.dy += this.velocity;
         }
-
-        if (this.x > windowWidth + this.radius) {
-            this.x =- this.radius;
+        if (this.x + this.radius > windowHeight || this.x - this.radius < 0) {
+            this.dx = - this.dx;
         }
     }
 }
 
-let balls = [];
-for (let i = 0; i < 10; i++)
-{
-    balls.push(new Ball());
+function init() {
+    for (let i = 0; i < 10; i++) {
+        balls.push(new Ball());
+    }
 }
 
+init();
+
 function animateBall() {
-    console.log("animating");
     requestAnimationFrame(animateBall);
-    balls[0].update();
-    balls[0].draw();
+    ctx.clearRect(0, 0, windowWidth, windowHeight);
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].update();
+        balls[i].draw();
+    }
 };
 
 animateBall();
