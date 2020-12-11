e01909becc3eb4da62d1a17b156ea1558a9f7fc7
Jeannie An <jeanniehjan@gmail.com>
2020-12-11 15:01:46 +0000

    added Scroll button for line width

--- Day5/paint.js
+++ Day5/paint.js
@@ -1,18 +1,23 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const lineThickness = document.getElementsByClassName("slider")[0];
+
+console.log(lineThickness.value);
+
 let isDrawing = false;
+const BORDER_WIDTH = 16;
 
 // window on load
 window.addEventListener("load", () => {
     console.log("Window loaded");
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight / 2;
+    canvas.width = window.innerWidth / 2;
 });
 
 // resizing window
 window.addEventListener("resize", () => {
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight / 2;
+    canvas.width = window.innerWidth / 2;
 });
 
 // mouse down
@@ -27,22 +32,27 @@ canvas.addEventListener("mouseup", () => {
     ctx.beginPath();
 });
 
+// mouse move
 canvas.addEventListener("mousemove", event => {
     draw(event);
 });
 
+// value change in slider
+lineThickness.addEventListener('change', () => {
+    console.log(lineThickness.value);
+});
+
+
 function draw(event) {
     if (!isDrawing) return;
-    let mousePosX = event.clientX;
-    let mousePosY = event.clientY;
+    let lineWidth = lineThickness.value;
+
+    let mousePosX = event.clientX - BORDER_WIDTH;
+    let mousePosY = event.clientY - BORDER_WIDTH;
     
-    ctx.lineWidth = 10;
+    ctx.lineWidth = lineWidth;
     ctx.lineCap = "round";
     ctx.lineTo(mousePosX, mousePosY);
+    ctx.strokeStyle = "red";
     ctx.stroke();
-
-    // mousePosX = event.clientX;
-    // mousePosY = event.clientY;
-    // ctx.beginPath();
-    // ctx.moveTo(mousePosX, mousePosY);
 }
