9d2020e2cf44bf246fe3ffa3c9e9072fff58e9c2
Jeannie An <jeanniehjan@gmail.com>
2020-12-22 13:29:40 +0000

    refactoring

--- Day6/toDoList.js
+++ Day6/toDoList.js
@@ -3,98 +3,84 @@ const toDoInput = document.querySelector("#todo");
 const toDoButton = document.querySelector(".todo-button");
 const toDoList = document.querySelector(".todo-list");
 const toDoForm = document.querySelector("#form");
-// let deleteButtons = document.querySelectorAll(".delete-button");
-// todoButton.addEventListener("click", addTodo);
-// todoList.addEventListener("click", deleteTodo);
-// filterOption.addEventListener("click", filterTodo);
-
 let deleteButtons = document.querySelectorAll(".delete-button");
 
 toDoList.addEventListener("click", deleteToDo);
+toDoButton.addEventListener("click", addToDo);
+toDoForm.addEventListener("submit", ignoreFormSubmit);
+
+function ignoreFormSubmit(e) {
+  e.preventDefault();
+  e.stopPropagation();
+}
+
+let makeCounter = function () {
+  let privateCounter = 1;
+  function changeBy(val) {
+    privateCounter += val;
+  }
+  return {
+    increment: function () {
+      changeBy(1);
+    },
+    decrement: function () {
+      changeBy(-1);
+    },
+    value: function () {
+      return privateCounter;
+    },
+  };
+};
+
+let counter = makeCounter();
 
 function deleteToDo(e) {
   const item = e.target.closest("button");
   const parent = item.parentElement;
-  console.log(parent);
-
-  //   if (item.parentElement) {
-
-  //   }
-  //   console.log(item.parentElement.parentElement);
+  const parentClassName = parent.className;
+  const grandParent = parent.parentElement;
+  const grandParentClassName = grandParent.className;
+  const temp = document.getElementsByClassName(parentClassName)[0];
+  grandParent.removeChild(parent);
+  counter.decrement();
 }
-
-// function deleteTodo(e) {
-//     const item = e.target;
-
-//     if (item.classList[0] === "trash-btn") {
-//       // e.target.parentElement.remove();
-//       const todo = item.parentElement;
-//       todo.classList.add("fall");
-//       //at the end
-//       removeLocalTodos(todo);
-//       todo.addEventListener("transitionend", e => {
-//         todo.remove();
-//       });
-//     }
-//     if (item.classList[0] === "complete-btn") {
-//       const todo = item.parentElement;
-//       todo.classList.toggle("completed");
-//       console.log(todo);
-//     }
-//   }
-
+// probably dont need this function anymore?
 function updateDeleteButtonsList() {
   console.log("updating buttons list");
   deleteButtons = document.querySelectorAll(".delete-button");
-
-  deleteButtons.forEach((btn) => {
-    btn.addEventListener("click", (event) => {
-      //   console.log("delete btn clicked");
-      //   console.log(event.currentTarget);
-    });
-  });
 }
 
-function init() {
-  let tracker = 1;
+function addToDoItem(text) {
+  let div = document.createElement("div");
+  div.className = `todo-item item${counter}`;
 
-  toDoButton.addEventListener("click", () => {
-    let text = toDoInput.value;
-    addToDoItem(text);
-    updateDeleteButtonsList();
-    tracker++;
-  });
+  let label = document.createElement("label");
+  label.htmlFor = `item${counter}`;
+  label.innerHTML = text;
 
-  toDoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-  });
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.name = `item${counter}`;
+  checkbox.id = `item${counter}`;
 
-  function addToDoItem(text) {
-    let div = document.createElement("div");
-    div.className = `todo-item item${tracker}`;
+  let deleteBtn = document.createElement("button");
+  deleteBtn.className = "delete-button";
+  let deleteBtnIcon = document.createElement("i");
+  deleteBtnIcon.className = "far fa-trash-alt";
 
-    let label = document.createElement("label");
-    label.htmlFor = `item${tracker}`;
-    label.innerHTML = text;
+  deleteBtn.appendChild(deleteBtnIcon);
+  div.appendChild(label);
+  div.appendChild(checkbox);
+  div.appendChild(deleteBtn);
+  toDoList.append(div);
+}
 
-    let checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.name = `item${tracker}`;
-    checkbox.id = `item${tracker}`;
-
-    let deleteBtn = document.createElement("button");
-    deleteBtn.className = "delete-button";
-    let deleteBtnIcon = document.createElement("i");
-    deleteBtnIcon.className = "far fa-trash-alt";
-
-    deleteBtn.appendChild(deleteBtnIcon);
-
-    div.appendChild(label);
-    div.appendChild(checkbox);
-    div.appendChild(deleteBtn);
-    toDoList.append(div);
+function addToDo() {
+  let text = toDoInput.value;
+  if (!text) {
+    alert("Please enter a task name");
+    return;
   }
+  addToDoItem(text);
+  counter.increment();
 }
-
-init();
